c9310859d82c29c0daa7cdf000b25650ba91d504
SilvestreL <lucassilvestreadv@gmail.com>
2025-01-28 21:55:26 -0300

    feat: add stripe webhook that handles subsctiptions

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,17 +1,21 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_actions/create-checktou/_components/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const Subscription = async () => {
   const { userId } = await auth();
-  if (!userId) return redirect("/login");
+  if (!userId) redirect("/login");
+
+  const user = await clerkClient().users.getUser(userId);
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
 
   return (
     <>
-      <Navbar />;
+      <Navbar />
       <div className="space-y-6 p-6">
         <h1 className="text-2xl font-bold">Assinaturas</h1>
         <div className="flex gap-6">
@@ -21,9 +25,9 @@ const Subscription = async () => {
                 Plano Básico
               </h2>
               <div className="flex items-center justify-center gap-3">
-                <span className="text-3xl">R$</span>
-                <span className="font-semibold">0</span>
-                <div className="text-2xl text-muted-foreground">/mës </div>
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">0</span>
+                <div className="text-2xl text-muted-foreground">/mês </div>
               </div>
             </CardHeader>
             <CardContent className="space-y-6 py-8">
@@ -40,14 +44,19 @@ const Subscription = async () => {
 
           {/* card 2 */}
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-4 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
               <div className="flex items-center justify-center gap-3">
-                <span className="text-3xl">R$</span>
-                <span className="font-semibold">19</span>
-                <div className="text-2xl text-muted-foreground">/mës </div>
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">19</span>
+                <div className="text-2xl text-muted-foreground">/mês</div>
               </div>
             </CardHeader>
             <CardContent className="space-y-6 py-8">
